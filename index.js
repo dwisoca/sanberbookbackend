81523dcd830176359cd086ad4cd55a0312d72e00
soca <dwisoca@gmail.com>
2024-01-25 03:24:24 +0700

    Feat: Category (Add, Update, Delete, Get)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Firebase
 const admin = require("firebase-admin");
+const { getFirestore, FieldValue } = require('firebase-admin/firestore');
 const serviceAccount = require("./serviceAccountKey.json");
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-const dbFirestore = admin.firestore();
+const dbFirestore = getFirestore();
 const auth = admin.auth()
 
 // Express
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 // Express Middleware
 const isAuthenticated = async (req, res, next) => {
     try {
-        const { token, categoryName } = req.body;
-        console.log(categoryName)
+        const { token } = req.body;
         const decodedToken = await auth.verifyIdToken(token)
         next();
     } catch (error) {
@@ -31,7 +30,14 @@ const isAuthenticated = async (req, res, next) => {
 
 // GET /categories
 app.get('/categories', async (req, res) => {
-  res.send('GET REQ Succeed')
+  const collectionRef = dbFirestore.collection('category')
+  const snapshot = await collectionRef.get();
+  let result = []
+  snapshot.forEach(doc => {
+    result.push(doc.data())
+  });
+  // console.log(result)
+  res.send(result)
 });
 
 // GET /categories/:id/books
@@ -41,19 +47,26 @@ app.get('/categories/:id/books', async (req, res) => {
 
 // POST /categories
 app.post('/categories', isAuthenticated, async (req, res) => {
-  const { token, categoryName } = req.body;
+  const { categoryName } = req.body;
   await addCategory(categoryName)
   res.send('Sukses menambah kategori: ' + categoryName)
 });
 
 // PATCH /categories/:id
 app.patch('/categories/:id', isAuthenticated, async (req, res) => {
-  // ...
+  const { categoryName } = req.body;
+  const categoryID = req.params.id;
+
+  await updateCategory(categoryID, categoryName)
+  res.send('Sukses update kategori ID: ' + categoryID)
 });
 
 // DELETE /categories/:id
 app.delete('/categories/:id', isAuthenticated, async (req, res) => {
-  // ...
+  const categoryID = req.params.id;
+
+  await deleteCategory(categoryID)
+  res.send('Sukses delete kategori ID: ' + categoryID)
 });
 
 
@@ -64,17 +77,38 @@ app.listen(port, () => {
 
 async function addCategory(categoryName){
   console.log(categoryName)
-  // Baca data untuk membuat nomor urut
+  // Count collection for ID's
   const collectionRef = dbFirestore.collection('category');
   const snapshot = await collectionRef.count().get();
-  const categoryID = snapshot.data().count + 1
-  console.log(categoryID);
+  const categoryID = (snapshot.data().count + 1).toString()
+  // console.log(categoryID);
 
-  const data = {
+  // Write to db
+  const targetRef = dbFirestore.collection('category').doc(categoryID);
+  const res = await targetRef.set({
     id: categoryID,
     name: categoryName,
-    created_at: Timestamp,
-    updated_at: Timestamp,
-  };
-  const res = await dbFirestore.collection('category').doc(categoryID).set(data);
+    created_at: FieldValue.serverTimestamp(),
+    updated_at: FieldValue.serverTimestamp(),
+  });
+  
+}
+
+async function updateCategory(categoryID, categoryName){
+  console.log('Update category ID:', categoryID)
+
+  // Write to db
+  const targetRef = dbFirestore.collection('category').doc(categoryID.toString());
+  const res = await targetRef.update({
+    name: categoryName,
+    updated_at: FieldValue.serverTimestamp(),
+  });
+  
+}
+
+async function deleteCategory(categoryID){
+  console.log('Delete category ID:', categoryID)
+
+  const res = await dbFirestore.collection('category').doc(categoryID.toString()).delete();
+  
 }
